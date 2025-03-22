119910a0246e5cfaffba2c290db910fea0c772cc
vidhu <vidhudash@gmail.com>
2025-03-22 13:45:18 +0530

    feat: server, servemux, handler

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// handler
+func home(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Snippetbox"))
+}
 
 func main() {
-    fmt.Println("lets go")
+    // servemux/router
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", home)
+
+    log.Print("Starting server on :4000")
+    // server
+    err := http.ListenAndServe(":4000", mux)
+    log.Fatal(err)
 }
