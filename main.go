e2a85b2ca621c61ea08c6f8398849738c28b26aa
vidhu <vidhudash@gmail.com>
2025-03-22 14:02:22 +0530

    feat: add more handlers

--- main.go
+++ main.go
@@ -7,13 +7,27 @@ import (
 
 // handler
 func home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
     w.Write([]byte("Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("View snippets"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("Create snippets"))
 }
 
 func main() {
     // servemux/router
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
+    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/snippet/create", snippetCreate)
 
     log.Print("Starting server on :4000")
     // server
