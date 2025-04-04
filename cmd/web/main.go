7c0e02da9c2291c79d3c4ce6d6b57407e4e1a688
vidhu <vidhudash@gmail.com>
2025-04-05 01:38:06 +0530

    feat: add logging levels, DI

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,38 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	infoLog *log.Logger
+	errorLog *log.Logger
+}
+
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate | log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate | log.Ltime | log.Lshortfile)
+
+	app := &application {
+		infoLog: infoLog,
+		errorLog: errorLog,
+	}
+
     mux := http.NewServeMux()
 
     fileServer := http.FileServer(http.Dir("./ui/static/"))
-
     mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet/view", snippetView)
+    mux.HandleFunc("/", app.home)
+    mux.HandleFunc("/snippet/view", app.snippetView)
 
-    mux.HandleFunc("POST /snippet/create", snippetCreate)
+    mux.HandleFunc("POST /snippet/create", app.snippetCreate)
     mux.HandleFunc("/snippet/create", func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Allow", http.MethodPost)
         w.Header().Set("Content-Type", "text/html")
@@ -23,7 +40,13 @@ func main() {
         return
     })
 
-    log.Print("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	srv := &http.Server{
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+
+    infoLog.Printf("Starting server on %v\n", *addr)
+    err := srv.ListenAndServe()
+    errorLog.Fatal(err)
 }
